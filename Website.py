425f12e9d0014c41656aacf0792e31438abcb850
vcvycy <352871242@qq.com>
2018-11-02 15:38:03 +0800

    x

--- Website.py
+++ Website.py
@@ -1,5 +1,19 @@
 from XMULogin import XMULogin
 from bottle import *
+
+@get("/")
+def index():
+    # print(os.curdir)
+    return static_file("index.html","www")
+@get("/login")
+def login():
+    sid = request.query.sid
+    pwd = request.query.pwd
+    print("%s %s" %(sid,pwd))
+    if xmu.login(sid,pwd):
+        return "<h1>%s 登陆成功</h1>" %(sid)
+    else:
+        return "<h1>登陆失败 : %s </h1> " %(xmu.loginFailedReason)
 if __name__ == "__main__":
-    xmu=XMULogin()
-    print(xmu.login("23020161153315", "159147"))
+    xmu = XMULogin()
+    run(host="0.0.0.0", port=81)
